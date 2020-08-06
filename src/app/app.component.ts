e00171a60779d6590f6663a9d82feac65f95c86c
Julie Larkin <julielarkin@Julies-MacBook-Air.local>
2020-08-05 19:58:32 -0700

    Completed parts 8, bonus mission A

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,13 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
-  //displayList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
-    //this.displayList = [];
+    
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
           let fetchedSatellites = data.satellites;
@@ -26,12 +27,12 @@ export class AppComponent {
           // TODO: loop over satellites
           // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
-          //this.displayList = this.sourceList.slice(0);
+          this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
  
  }
- /*search(searchTerm: string): void {
+ search(searchTerm: string): void {
   let matchingSatellites: Satellite[] = [];
   searchTerm = searchTerm.toLowerCase();
   for(let i=0; i < this.sourceList.length; i++) {
@@ -39,9 +40,9 @@ export class AppComponent {
      if (name.indexOf(searchTerm) >= 0) {
         matchingSatellites.push(this.sourceList[i]);
      }
-  }*/
+  }
   // assign this.displayList to be the array of matching satellites
   // this will cause Angular to re-make the table, but now only containing matches
-  //this.displayList = matchingSatellites;
+  this.displayList = matchingSatellites;
 }
-//}
+}
